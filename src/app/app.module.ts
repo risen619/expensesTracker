c1a7806d1f52653f8e669b422745190bdd01f2c8
Andriy Komar <risen619@gmail.com>
2018-04-10 14:07:18 +0300

    home page markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -12,29 +11,26 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        TabsPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        ReactiveFormsModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        TabsPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: ErrorHandler, useClass: IonicErrorHandler }
+    ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,92 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage
+{
+    form: FormGroup;
 
-  constructor(public navCtrl: NavController) {
+    categoryChosen: boolean = false;
+    categories = ['General', 'Transport', 'Special', 'Medicine', 'Sweet\'s', 'Coffee', 'Fast food'];
+    filteredCategories: string[] = [];
 
-  }
+    expenses = [];
 
+    constructor(
+        public navCtrl: NavController,
+        private fb: FormBuilder
+    )
+    {
+        this.createForm();
+    }
+
+    createForm()
+    {
+        let currencyPattern = /^[1-9]\d*\.?\d{0,2}$/;
+        this.form = this.fb.group({
+            expenseName: '',
+            expenseCategory: '',
+            expenseAmount: ['', Validators.compose([Validators.required, Validators.pattern(currencyPattern)])]
+        });
+        window['home'] = this;
+    }
+
+    get name() : AbstractControl { return this.form.get('expenseName'); }
+    get category() : AbstractControl { return this.form.get('expenseCategory'); }
+    get amount() : AbstractControl { return this.form.get('expenseAmount'); }
+
+    addCategory(category)
+    {
+        this.categories.push(category);
+        this.categoryChosen = true;
+    }
+
+    addExpense()
+    {
+        if(this.form.invalid) return;
+        let expense = {
+            name: this.name.value || 'Unnamed expense',
+            category: this.category.value || 'Uncategorized',
+            amount: this.amount.value
+        };
+        console.log("Expense would be like: ", expense);
+        this.expenses.push(expense);
+    }
+
+    chooseCategory(category)
+    {
+        this.category.setValue(category);
+        this.categoryChosen = true;
+        this.filterCategories();
+    }
+
+    filterCategories()
+    {
+        if(this.categoryChosen)
+        {
+            this.filteredCategories = [];
+            return;
+        }
+
+        this.filteredCategories = this.categories.filter(v => v.toLowerCase().includes(this.category.value.toLowerCase()));
+        if(this.filteredCategories.length > 3)
+            this.filteredCategories = this.filteredCategories.slice(0, 3);
+    }
+
+    handleInput()
+    {
+        if(this.category.value && this.category.value.trim() === "") return;
+        if(this.categoryChosen)
+            this.categoryChosen = false;
+        this.filterCategories();
+    }
+
+    currencyCheck()
+    {
+        console.log("!");
+    }
 }
